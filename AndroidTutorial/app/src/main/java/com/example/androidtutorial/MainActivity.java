abb987a607992f081a271db7661824bb0ef2774c
Hoang Thuan <thuanphakloi@gmail.com>
2019-07-04 21:08:18 +0700

    RelativeLayout and Object

--- AndroidTutorial/app/src/main/java/com/example/androidtutorial/MainActivity.java
+++ AndroidTutorial/app/src/main/java/com/example/androidtutorial/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androidtutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //TẠO ĐỐI TƯỢNG TỪ KHUÔN Student
+        
+        Student sv1 ; //B1: KHAI BÁO:
+        sv1 = new Student(); //B2: KHỞI TẠO:
+
+        //B3: GÁN GIÁ TRỊ:
+        sv1.name = "Tú";
+        sv1.age = 22;
+        sv1.schoolName = "Unknown";
+        sv1.id = 1997;
+
+        Toast.makeText(this, sv1.name, Toast.LENGTH_SHORT).show();
+        
     }
 }
---
+++ AndroidTutorial/app/src/main/java/com/example/androidtutorial/Student.java
@@ -0,0 +1,8 @@
+package com.example.androidtutorial;
+
+public class Student {
+    String name;
+    int age;
+    String schoolName;
+    int id;
+}
