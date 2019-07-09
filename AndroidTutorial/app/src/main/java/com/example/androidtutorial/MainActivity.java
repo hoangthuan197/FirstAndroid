10497e0038d4ab6a0a8eac1ae102ca53ae66ff67
Hoang Thuan <thuanphakloi@gmail.com>
2019-07-09 20:57:03 +0700

    09/07 Commit

--- AndroidTutorial/app/src/main/java/com/example/androidtutorial/MainActivity.java
+++ AndroidTutorial/app/src/main/java/com/example/androidtutorial/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.androidtutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,19 +15,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //TẠO ĐỐI TƯỢNG TỪ KHUÔN Student
-        
-        Student sv1 ; //B1: KHAI BÁO:
-        sv1 = new Student(); //B2: KHỞI TẠO:
+        init();
 
-        //B3: GÁN GIÁ TRỊ:
-        sv1.name = "Tú";
-        sv1.age = 22;
-        sv1.schoolName = "Unknown";
-        sv1.id = 1997;
+//        TẠO ĐỐI TƯỢNG TỪ KHUÔN Student
+//
+//        Student sv1 ; //B1: KHAI BÁO:
+//        sv1 = new Student(); //B2: KHỞI TẠO:
+//
+//        //B3: GÁN GIÁ TRỊ:
+//        sv1.name = "Tú";
+//        sv1.age = 22;
+//        sv1.schoolName = "Nguyen Hue 2";
+//        sv1.id = 1997;
+//
+//        Toast.makeText(this, sv1.schoolName, Toast.LENGTH_SHORT).show();
+//
+    }
 
-        Toast.makeText(this, sv1.name, Toast.LENGTH_SHORT).show();
-        
+    public void init() {
+        // Khai bao cho nao~ biet dau la TextView co chu Hello
+        TextView tvHello;
+
+        // Khoi tao bang cach lien ket vs xml
+        tvHello = findViewById(R.id.tv_hello);
+
+        // Gan gia tri
+        tvHello.setText("Hello");
+
+        //Again
+        final EditText edtPhoneNumber;
+        edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        edtPhoneNumber.setHint("Your Phone Number");
+
+        TextView tvLogIn;
+        tvLogIn = findViewById(R.id.tv_login);
+        tvLogIn.setText("Login");
+
+        TextView tvLoginWithPn;
+        tvLoginWithPn = findViewById(R.id.tv_login_with_pn);
+        tvLoginWithPn.setText("Log in with your phone number");
+
+        TextView tvLoginBtn;
+        tvLoginBtn = findViewById(R.id.tv_login_btn);
+        tvLoginBtn.setText("Log in");
+
+        TextView tvSocialLogin;
+        tvSocialLogin = findViewById(R.id.tv_social_login);
+        tvSocialLogin.setText("With Socila Login");
+
+        RelativeLayout rlLoginButton;
+        rlLoginButton = findViewById(R.id.rl_login_btn);
+        rlLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = edtPhoneNumber.getText().toString();
+                int phoneNumberChecker = phoneNumber.length();
+                if (phoneNumberChecker > 9) {
+                    Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+
+                    Intent intent;
+                    intent = new Intent(MainActivity.this, UpdateUserInfoActivity.class);
+                    startActivity(intent);
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Your phone number is too short", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
